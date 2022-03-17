53936e4f4a28c30ee9548aebd49063ce215888b8
Grant Jurgensen <gajurgensen@gmail.com>
2022-03-17 16:30:11 -0500

    Improve ch1 parser

--- ch1/src/Parse.hs
+++ ch1/src/Parse.hs
@@ -9,48 +9,53 @@ import Text.Parsec.String
 
 import Ast
 
--- keywords :: [String]
--- keywords = ["fn"]
 
 tok :: Parser a -> Parser a
-tok p = do
-    a <- p
-    spaces
-    pure a
+tok = (<* spaces)
+
+notBefore :: Show b => Parser a -> Parser b -> Parser a
+p `notBefore` q = p <* notFollowedBy q
 
 symbol :: String -> Parser String
-symbol = tok . string
+symbol = tok . try . string
+
+keyword :: String -> Parser String
+-- Keyword is a symbol which is ambigious with identifiers
+keyword s = tok $ try $ string s `notBefore` identChar
 
 symbols :: [String] -> Parser String
 symbols = choice . fmap symbol
 
-ident :: Parser String
-ident = tok ( do 
+parens :: Parser a -> Parser a
+parens = between (symbol "(") (symbol ")")
+
+identChar :: Parser Char
+identChar = alphaNum <|> oneOf ['-', '_', '\'']
+
+keywords :: [String]
+keywords = ["fn"]
+
+ident :: Parser Ident
+ident = tok (try $ do 
     c  <- lower
-    cs <- many $ alphaNum <|> oneOf ['-', '_', '\'']
-    pure $ c:cs
+    cs <- many identChar
+    let str = c:cs
+    if str `elem` keywords then
+        unexpected "keyword"
+    else pure str
     ) <?> "identifier"
 
 var :: Parser Term
 var = Var <$> ident
 
-followedBy :: Parser a -> Parser b -> Parser a
-p `followedBy` q = do 
-    x <- p
-    q 
-    pure x
-
 lambda :: Parser Term
 lambda = tok ( do 
-    symbol "λ" <|> (string "fn" `followedBy` many1 space)
+    symbol "λ" <|> keyword "fn"
     args <- many1 ident
     symbol "."
     body <- term
     pure $ foldr (Lambda Map.empty) body args
     ) <?> "lambda expression"
-
-parens :: Parser a -> Parser a
-parens = between (symbol "(") (symbol ")")
 
 term :: Parser Term
 term = do 
@@ -59,10 +64,9 @@ term = do
     pure $ foldl App hd tl
     <?> "term"
   where
-    terms   = many1 subterm
     subterm = parens term <|> lambda <|> var
 
 parser :: Parser Term
 parser = do
     spaces
-    term `followedBy` eof
+    term <* eof
